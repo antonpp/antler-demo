8d49254124d00d62ef8b7dcaa6a49a166456b427
Anton <popovine@google.com>
2024-04-08 11:55:29 +0000

    yell

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     city = request.args.get('city')
-    return f"Welcome to {city}!"
+    return f"Welcome to {city}!!!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
